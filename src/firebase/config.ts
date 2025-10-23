e498ff8e60a9bff14ca4369cdd830977fb36fd85
Benz Siangco <benzsiangco@gmail.com>
2025-10-23 09:58:51 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8370891772-5528f",
+  "appId": "1:262302775263:web:3eded0935b00766e3f366a",
+  "apiKey": "AIzaSyDG3a5DGtC2_vgIudIJaRbFSu7NDa8AZ-g",
+  "authDomain": "studio-8370891772-5528f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "262302775263"
+};
